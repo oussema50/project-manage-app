b798aa45d3202b1095ea28106e295695fd47a883
oussema <omranioussema3@gmail.com>
2024-11-11 13:23:54 +0100

    comment

--- backend/controllers/workingHoursController.js
+++ backend/controllers/workingHoursController.js
@@ -22,6 +22,7 @@ exports.createWorkingHour = asyncHandler(async(req,res,next)=>{
     const existingWorkingHour = await WorkingHours.findOne({
         where: {
             employeeId,
+            status:'pending'
         },
         order: [['createdAt', 'DESC']]
     });
@@ -33,9 +34,67 @@ exports.createWorkingHour = asyncHandler(async(req,res,next)=>{
         }
     }
     const workingHour = await WorkingHours.create({ employeeId, date, startTime, endTime, hoursOfWork:hours});
-    res.status(201).json(workingHour)
+    res.status(201).json({data:workingHour})
 });
 
-exports.updateWorkingHour = asyncHandler(async(req,res,next)=>{
+// @desc    update the status of working hours
+// @route   PUT /api/v1/working-hour/update-working-hour/{id}
+// @access   Private RH 
+exports.updateStatusWorkingHour = asyncHandler(async(req,res,next)=>{
     const {status} = req.body
+    const {id} = req.params
+    const workingHours = WorkingHours.findOne()
+})
+
+// @desc    Get All working hours
+// @route   get /api/v1/working-hour/all-working-hours
+// @access   Private RH
+exports.getAllWorkingHours = asyncHandler(async(req,res,next)=>{
+    const { status, date,hoursOfWork, page = 1, limit = 3 } = req.query;
+    const pageNumber = parseInt(page, 10);
+    const pageSize = parseInt(limit, 10);
+    const searchCriteria = {
+        ...(status && { status: { [Op.eq]: `%${status}%` } }),
+        ...(date && { date: { [Op.eq]: `%${date}%` } }), 
+        ...(hoursOfWork && { hoursOfWork: { [Op.eq]: hoursOfWork } }),  
+      };
+    const { count, rows } = await WorkingHours.findAndCountAll({
+    where: searchCriteria,
+    limit: pageSize,          // Number of records to return
+    offset: (pageNumber - 1) * pageSize, // Calculate offset
+    });
+    const totalPages = Math.ceil(count / pageSize);
+    res.json({
+        totalUsers: count,
+        totalPages,
+        currentPage: pageNumber,
+        workingHours: rows,
+      });
+})
+
+// @desc    Get working Hours by employee Id
+// @route   get /api/v1/working-hour/id
+// @access   Private RH or Employee
+exports.getWorkingHoursById = asyncHandler(async(req,res,next)=>{
+    const {id} = req.params
+    const { status, date,hoursOfWork, page = 1, limit = 3 } = req.query;
+    const pageNumber = parseInt(page, 10);
+    const pageSize = parseInt(limit, 10);
+    const searchCriteria = {
+        ...(status && { status: { [Op.eq]: `%${status}%` } }),
+        ...(date && { date: { [Op.eq]: `%${date}%` } }), 
+        ...(hoursOfWork && { hoursOfWork: { [Op.eq]: hoursOfWork } }),  
+      };
+    const { count, rows } = await WorkingHours.findAndCountAll({
+    where: {employeeId:id,...searchCriteria},
+    limit: pageSize,          // Number of records to return
+    offset: (pageNumber - 1) * pageSize, // Calculate offset
+    });
+    const totalPages = Math.ceil(count / pageSize);
+    res.json({
+        totalUsers: count,
+        totalPages,
+        currentPage: pageNumber,
+        workingHours: rows,
+      });
 })
--- backend/router/workingHourRoute.js
+++ backend/router/workingHourRoute.js
@@ -2,10 +2,12 @@ const passport = require('passport');
 const express = require('express');
 const {isRH,isEmployee} = require('../middlewares/protectedRoute');
 const {passportScr }=require('../utils/passport');
-const {createWorkingHour} = require('../controllers/workingHoursController')
+const {createWorkingHour,getAllWorkingHours,getWorkingHoursById,updateStatusWorkingHour} = require('../controllers/workingHoursController')
 const {postCreateHoursValidator} = require('../utils/validators/workingHoursValidator');
 passportScr(passport)
 const router = express.Router()
+router.get('/all-working-hours',passport.authenticate('jwt', { session: false }),isRH,getAllWorkingHours)
+router.get('/update-status/:id',passport.authenticate('jwt', { session: false }),isRH,getAllWorkingHours)
 router.post('/',passport.authenticate('jwt', { session: false }),postCreateHoursValidator,createWorkingHour)
 
 module.exports = router;
